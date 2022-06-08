f8537c389a8f9dc76f6165ac53f873de862b8cdd
owenyuwono <michael.oswin@ovo.id>
2022-06-08 21:54:48 +0700

    feat: add mainnet contract_address

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 const config = {
     1: {
-        contract_address: '0x51891543342C9e0Dd75429CDC8f42cf66f61e117'
+        contract_address: '0xa626F0c2d01281D9f82bfb47Eb39f5Ef66a92d17'
     },
     4: {
         contract_address: '0x198A84955Ed779B74eBEf7Cb2DAb668c20388875'
