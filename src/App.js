dd5c1ff6cd63ddbac77ff1f0d893a8fbaffaa1d4
owenyuwono <michael.oswin@ovo.id>
2022-06-08 11:48:41 +0700

    fix: lint

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Navbar from './components/Navbar';
+// import Navbar from './components/Navbar';
 import Web3 from "web3";
 import { useEffect, useState } from 'react';
 import build from "./contracts/WZKD.json";
